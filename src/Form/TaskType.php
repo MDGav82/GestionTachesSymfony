c223cfce8ca9b957a55c2b462f65c9f1da6ea65a
ABENOJQ <quentina344@gmail.com>
2025-03-17 11:14:15 +0100

    feat(controller) :  ajout controller crud

---
+++ src/Form/TaskType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\project;
+use App\Entity\Task;
+use App\Entity\user;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TaskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('state')
+            ->add('progress_percent')
+            ->add('start_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('end_date', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('associated_project', EntityType::class, [
+                'class' => project::class,
+                'choice_label' => 'id',
+            ])
+            ->add('associated_user', EntityType::class, [
+                'class' => user::class,
+                'choice_label' => 'id',
+                'multiple' => true,
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Task::class,
+        ]);
+    }
+}
